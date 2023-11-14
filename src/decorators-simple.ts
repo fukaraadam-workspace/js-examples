dea405eea42bfe0e15542f4cb9f6d0d7cd526420
Furkan Alan <furkanalan@hotmail.com>
2023-11-14 17:26:16 +0300

    added first example

---
+++ src/decorators-simple.ts
@@ -0,0 +1,16 @@
+/**
+ * Example of a simple decorator
+ * Also known as a higher order function
+ */
+
+const logger = (message) => console.log(message);
+
+function loggerDecorator(logger) {
+  return function (message) {
+    logger.call(this, message);
+    console.log("message logged at:", new Date().toLocaleString());
+  };
+}
+
+const decoratedLogger = loggerDecorator(logger);
+decoratedLogger("Hello World");
